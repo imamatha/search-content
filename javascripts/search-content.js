9bfc72aa4a75bf7863da2e6ee3b7a1129840d45d
mamatha <munivelu.mamatha@accenture.com>
2012-10-15 11:26:43 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -113,12 +113,13 @@ function search() {
                     discussion +='</ul>';
                    
                     discussion +='<ul>';
-                    discussion +='<li>'created:'</li>';
+                    discussion +='<li>created:</li>';
                    // discussion +='<li><img src="'+ avatar + '" width=\'25px\' height=\'25px\' border=\'0\'/></li>';
                     //discussion +='<li><a href=https://apps-onprem.jivesoftware.com/people/'+username+'>'+author+'</a></li>';
                    discussion +='<li>'+createdDate+'</li>';
-                   discussion +='<li>'Last Modified:'<li>';
+                   discussion +='<li>Last Modified:<li>';
                    discussion +='<li>'+modifiedDate+'</li>';
+                     discussion +='<li>Likes:<li>';
                    discussion +='<li>'+likeCount+'</li>';
                   // discussion +='<li>'+modifiedDate+'</li>';
                    discussion +='</ul>';
