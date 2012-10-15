8fe0315f932e2620bb3701c25229a26ca3d37c9a
mamatha <munivelu.mamatha@accenture.com>
2012-10-15 15:20:14 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -160,7 +160,7 @@ function search() {
 			   if(row.type=="update")
                {
                      update +='<ul>';
-                     update +='<li class="update" >'+contentsummary'</li>';
+                     update +='<li class="update" ><a href="'+url+'">'+contentSummary+'</a></li>';
                      update +='</ul>';
                     
                      update +='<h5>';
