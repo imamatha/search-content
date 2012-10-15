1dfd6395290362f7258e7e00cdc28ca4735fce0d
mamatha <munivelu.mamatha@accenture.com>
2012-10-15 18:10:03 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -99,31 +99,34 @@ function search() {
                    
 		    discussion +='<ul>';
                     discussion +='<li class="discussion" ><a href="'+url+'" target="_apps">'+subject+'</a></li>';
-                    discussion +='</ul>';              
-                                   
+                    discussion +='</ul>';  
+                    
+                    discussion +='<font size="2" color="grey">';               
                     discussion +='<ul>';
-                    discussion +='<font size="2" color="grey">';
+                   
                     //discussion +='<li>&nbsp;</li>';
                   //  discussion +='<li>Created by<img src="'+ avatar + '" width=\'25px\' height=\'25px\' border=\'0\'/>
                     discussion +='<li>Created by <a class="nopad" href=https://apps-onprem.jivesoftware.com/people/'+username+'>'+author+'</a></li>';
                     //console.log("Author: "+author);
-                    discussion +='</font>';
+                   
                     discussion +='</ul>';
+                     discussion +='</font>';
                                                       
                     discussion +='<ul>';
                    // discussion +='<b>';
                     discussion +='<li>'+contentSummary+'</li>';
                     //discussion +='</b>';
                     discussion +='</ul>';
-                                                      
+                    
+                    discussion +='<font size="2" color="grey">';                                 
                     discussion +='<ul>';   
                     discussion +='<font size="2" color="grey">';
                     discussion +='<li>Created:'+creationDate+'</li>';
                     discussion +='<li>Last Modified:'+modifiedDate+'</li>';                
                     discussion +='<li>Replies:'+replyCount+'</li>';                  
-                    discussion +='<li>Likes:'+likeCount+'</li>';\
-                    discussion +='</font>';
+                    discussion +='<li>Likes:'+likeCount+'</li>';
                     discussion +='</ul>';
+                    discussion +='</font>';
                  
                     
                   //  discussion +='</h5>';
