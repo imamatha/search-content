e276002d1f1ef62611d5cc25ddb890691e9dcc9c
mamatha <munivelu.mamatha@accenture.com>
2012-10-16 08:46:05 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -110,11 +110,11 @@ function search() {
                      discussion +='</ul>';
                      discussion +='</font>';
                                                       
-                    discussion +='<ul>';
+                   // discussion +='<ul>';
                    // discussion +='<b>';
                     discussion +='<li>'+contentSummary+'</li>';
                     //discussion +='</b>';
-                    discussion +='</ul>';
+                    //discussion +='</ul>';
                     
                     discussion +='<font size="2" color="grey">';                                 
                     discussion +='<ul>';   
