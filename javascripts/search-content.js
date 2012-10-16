bb92437fa95643a4095948a8f95631112daa625c
mamatha <munivelu.mamatha@accenture.com>
2012-10-16 09:57:46 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -97,7 +97,7 @@ function search() {
 			   if(row.type=="discussion")
                {
                	    
-                    discussion +='<table>';
+                    discussion +='<table border= "">';
                     discussion +='<ol>';
 		    discussion +='<ul>';
                     discussion +='<li class="discussion"><a href="'+url+'" target="_apps">'+subject+'</a></li>';
