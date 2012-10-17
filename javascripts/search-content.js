a1f978974cd3675fc0390bd6f972bc31b0b2e395
mamatha <munivelu.mamatha@accenture.com>
2012-10-17 11:55:39 +0530

    Update javascripts/search-content.js

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -280,7 +280,7 @@ var newDate=finalMonth+" "+myDate[2]+","+myDate[0];
                     // post +='<div align=left>';
                      post +='<font size="2" color="black">';  
                      post +='<ul>';  
-                     post +='<li class="align">'+contentSummary+'</li>';  
+                     post +='<div>'+contentSummary+'</div>';  
                      post +='</ul>';
                      post +='</font>';  
                     // post +='</div>';
